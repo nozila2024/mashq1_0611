e545070f109c4fbd989fc7ab58fc855027479ed0
nozila2024 <nozilarajabboyeva@gmail.com>
2025-11-06 14:07:31 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+#1-misol
+for i in range(1, 101):
+    if i == 25:
+        print(i)
+        break
+
+#2-misol
+for i in range(1, 21):
+    if i % 7 == 0:
+        print(i)
+        break
+
+#3-misol
+for i in range(1, 21):
+    if i % 5 == 0:
+        print(i)
+        break
+
+#4-misol
+for i in range(1, 31):
+    print(i)
+    if i == 15:
+        print("To‘xtadik!")
+        break
+
+#5-misol
+for i in range(1, 51):
+    print(i)
+    if i % 5 == 0:
+        break
+
+#6-misol
+for i in range(1, 101):
+    print(i)
+    if i == 33:
+        print("33 soni topildi!")
+        break
+
+#7-misol
+for i in range(1, 21):
+    print(i)
+    if i > 10:
+        break
+
+#8-misol
+n = int(input("Biror chegara son kiriting: "))
+
+for i in range(1, 101):
+    print(i)
+    if i > n+1:
+        break
+
+#9-misol
+for i in range(1, 201):
+    print(i)
+    if i == 77:
+        print("77 soni topildi!")
+        break
+
+#10-misol
+for i in range(1, 30):
+    print(i)
+    if i % 3 == 0 and i % 5 == 0:
+        break
